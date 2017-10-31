6f3d37ac183d3fdf789ca4fe89aaf3572917ce1e
Brennan <bmart2913@gmail.com>
2017-10-31 09:11:40 -0400

    inti commit

---
+++ index.js
@@ -0,0 +1,40 @@
+
+// $(".available").on("click",function(){
+
+//     $(".popup").css("display","flex");
+//     $( this ).toggleClass( "reserved" );
+
+// });
+
+
+// $(".cancel").on("click",function(){
+
+// 	$(".popup").css("display","none");
+//     $( this ).toggleClass( "available" );
+
+// });
+
+
+let table1 = $( "table1" );
+
+
+// $(table1).on("click", function(){
+//     alert(table1 + " was clicked.");
+// }); 
+
+
+if ($(".available").on("click", function(){
+	debugger;
+	alert("yo");
+
+}));
+
+
+// let tableDetails {
+
+// 	tableNumber:,
+// 	reservationName:,
+// 	phoneNumber:,
+// 	groupSize:0
+
+// }
